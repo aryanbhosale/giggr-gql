57a79b4392606e1f7ebb58c943cf9964ff2c23cf
Aryan Bhosale <36108149+Spartan0070@users.noreply.github.com>
2023-07-14 13:35:09 +0530

    ui

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
                 <Link href="/">
-                    <span className='cursor-pointer font-bold text-4xl text-white'>
+                    <span className='cursor-pointer font-bold text-4xl text-gray-800'>
                         Giggr
                     </span>
                 </Link>
@@ -20,7 +20,7 @@ const Header = () => {
             <div className='hidden mb:float-left md:contents'>
                 {categories.map((category) => (
                     <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semiboldcursor-pointer'>
+                        <span className='md:float-right mt-2 align-middle text-gray-800 ml-4 font-semiboldcursor-pointer'>
                             {category.name}
                         </span>
                     </Link>
